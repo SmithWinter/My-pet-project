67d389bec49c0bb46176633d5b6454bee20d1f8f
Smith Winter <hovuminhduc@gmail.com>
2024-05-16 09:56:27 +0700

    Revert "Update entrypoint.sh"
    This reverts commit 602beb5421bbbd0239d8dafbfeb2a6fe066b6be4.

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,33 +6,33 @@ DIR_RUNTIME="/usr/bin"
 DIR_TMP="$(mktemp -d)"
 
 
-# cat << EOF > ${DIR_TMP}/heroku.json
-# {
-#     "inbounds": [{
-#         "listen": "0.0.0.0",
-#         "port": "${PORT}",
-#         "protocol": "vmess",
-#         "settings": {
-#             "clients": [{
-#                 "id": "${ID}",
-#                 "alterId": ${AID}
-#             }]
-#         },
-#         "streamSettings": {
-#             "network": "ws",
-#             "wsSettings": {
-#                 "path": "/"
-#             }
-#         },
-#         "sniffing": {
-#             "enabled": false
-#         }
-#     }],
-#     "outbounds": [{
-#         "protocol": "freedom"
-#     }]
-# }
-# EOF
+cat << EOF > ${DIR_TMP}/heroku.json
+{
+    "inbounds": [{
+        "listen": "0.0.0.0",
+        "port": "${PORT}",
+        "protocol": "vmess",
+        "settings": {
+            "clients": [{
+                "id": "${ID}",
+                "alterId": ${AID}
+            }]
+        },
+        "streamSettings": {
+            "network": "ws",
+            "wsSettings": {
+                "path": "/"
+            }
+        },
+        "sniffing": {
+            "enabled": false
+        }
+    }],
+    "outbounds": [{
+        "protocol": "freedom"
+    }]
+}
+EOF
 
 curl --retry 10 --retry-max-time 60 -H "Cache-Control: no-cache" -fsSL github.com/v2fly/v2ray-core/releases/download/v4.44.0/v2ray-linux-64.zip -o ${DIR_TMP}/v2ray_dist.zip
 busybox unzip ${DIR_TMP}/v2ray_dist.zip -d ${DIR_TMP}
